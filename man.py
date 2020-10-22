85ec553739be8a792e08d81899e9610293cc9278
Ahmed <ahmed.h.dahir@gmail.com>
2020-10-22 12:44:24 -0400

    New branch

--- man.py
+++ man.py
@@ -1,13 +1,17 @@
 print('Hello World')
 def main(msg):
-
+    # No longer  need comments
     # FartButt
 // Butt Fart
 
     print(msg)
 
+def secondary():
+    print('New Fucntion')
+
 
 main('Hello people')
+secondary()
 a=2+2
 b=a+9
 print(b)
